47858d102f175ff9b2640ff4ac7fddd306f6d7f9
skarakash <karakash.sergey@gmail.com>
2023-03-08 17:54:08 +0200

    update .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,10 @@ dotenv.config()
 
 const MONGO_PASSWORD = process.env.MONGOPASSWORD || "";
 const MONGO_PORT = process.env.MONGOPORT || 1337;
-const MONGO_URL = `mongodb://mongo:${MONGO_PASSWORD}@containers-us-west-73.railway.app:${MONGO_PORT}`;
+const MONGO_USER = process.env.MONGOUSER || "";
+const MONGO_HOST = process.env.MONGOHOST || "";
+
+const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}`;
 
 const SERVER_PORT = process.env.PORT ? process.env.PORT : Number(process.env.SERVER_PORT);
 
