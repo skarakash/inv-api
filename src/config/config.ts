e6c546e08b6465202d28701cea4b2ef6af8887ef
skarakash <karakash.sergey@gmail.com>
2023-03-08 17:45:42 +0200

    env variables fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
-const MONGO_PORT = process.env.MONGO_PORT || 1337;
+const MONGO_PASSWORD = process.env.MONGOPASSWORD || "";
+const MONGO_PORT = process.env.MONGOPORT || 1337;
 const MONGO_URL = `mongodb://mongo:${MONGO_PASSWORD}@containers-us-west-73.railway.app:${MONGO_PORT}`;
 
 const SERVER_PORT = process.env.PORT ? process.env.PORT : Number(process.env.SERVER_PORT);
